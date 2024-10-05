9e856b880705aad34b76a945d965662e0b4e8808
Jose Leon <leonj1@gmail.com>
2024-10-05 15:02:22 -0400

    FEAT Create VCS class for gitlab with interface. Preperation for github

---
+++ vcs/version_control.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+import math
+
+class VersionControl(ABC):
+    @abstractmethod
+    def domain(self) -> float:
+        """Return the domain for the version control system."""
+        pass
+
+    @abstractmethod
+    def project_path(self) -> float:
+        """Return the project path for the version control system."""
+        pass
+
+    @abstractmethod
+    def change_id(self) -> str:
+        """Return the merge request or pull request id for the version control system."""
+        pass
+    
